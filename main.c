11dbad57abddb75fc5da27325526d45c4c11059a
Greg Becker <greg.becker@klarasystems.com>
2024-01-19 10:13:26 -0600

    Add --hard-links and -H to usage output and man page.

--- main.c
+++ main.c
@@ -453,7 +453,7 @@ static void
 usage(int exitcode)
 {
 	fprintf(exitcode == 0 ? stdout : stderr, "usage: %s"
-	    " [-CDLPRSabcdgklnoprtuvx] [-e program] [-f filter] [--address=sourceaddr]\n"
+	    " [-CDHLPRSabcdgklnoprtuvx] [-e program] [-f filter] [--address=sourceaddr]\n"
 	    "\t[--append] [--bwlimit=limit] [--compare-dest=dir]\n"
 	    "\t[--del | --delete-before | --delete-during | --delete-after | --delete-during]\n"
 	    "\t[--delay-updates] [--dirs] [--no-dirs]\n"
