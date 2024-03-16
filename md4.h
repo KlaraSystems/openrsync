dbcb1a37539e5d77bccaae459b7bade958bae214
Allan Jude <allan@klarasystems.com>
2024-03-16 09:11:26 -0400

    MD4 compat

--- md4.h
+++ md4.h
@@ -28,6 +28,44 @@
 
 #define	MD4_DIGEST_LENGTH		16
 
+#ifdef __APPLE__
+#include <sys/param.h>
+
+/*
+ * Shim the MD4 implementation out to libmd, which in turn is largely just
+ * a CommonDigest shim.  The intermediate hop is just some macro definitions
+ * that are guaranteed to match what openrsync / openbsd expect and we won't
+ * actually need to link against libmd, so the indirection costs us  nothing.
+ */
+#include <md4.h>
+#include <limits.h>
+
+#define MD4_Init(ctx)			MD4Init(ctx)
+
+/*
+ * Shim MD4_Update out manually, because we have a slight size mismatch.
+ * CommonCrypto wants a CC_LONG, which is actually a uint32_t on all platforms,
+ * but we use size_t everywhere.
+ */
+static inline void
+MD4_Update(MD4_CTX *ctx, const void *data, size_t len)
+{
+	size_t resid;
+
+	while (len != 0) {
+		resid = MIN(len, UINT_MAX);
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+		MD4Update(ctx, data, resid);
+#pragma clang diagnostic pop
+		len -= resid;
+		data += resid;
+	}
+}
+
+#define	MD4_Final(digest, ctx)		MD4Final(digest, ctx)
+#else
 /* Any 32-bit or wider unsigned integer data type will do */
 typedef unsigned int MD4_u32plus;
 
@@ -41,5 +79,6 @@ typedef struct {
 extern void MD4_Init(MD4_CTX *ctx);
 extern void MD4_Update(MD4_CTX *ctx, const void *data, unsigned long size);
 extern void MD4_Final(unsigned char *result, MD4_CTX *ctx);
+#endif /* __APPLE__ */
 
 #endif
